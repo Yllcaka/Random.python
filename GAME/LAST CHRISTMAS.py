02f5790be193f9e579fb675f77374f41d7d3e405
Ylli Caka <yllcaka1516457@gmail.com>
2020-02-03 14:34:52 +0100

    Changed light balls in Tree

--- GAME/LAST CHRISTMAS.py
+++ GAME/LAST CHRISTMAS.py
@@ -37,19 +37,19 @@ red = []
 bblue = []
 def colored_dot(color):
     if color == "yellow":
-        return f"\u001b[33m◍\033[32m"
+        return f"\u001b[33m●\033[32m"
     if color == "blue":
-        return f"\u001b[34m◍\033[32m"
+        return f"\u001b[34m●\033[32m"
     if color == "red":
-        return f"\u001b[31m◍\033[32m"
+        return f"\u001b[31m●\033[32m"
     if color == "bblue":
-        return f"\u001b[37m◍\033[32m"
+        return f"\u001b[37m●\033[32m"
 
 def lights(color,indexes):
     off = True
     while True:
         for idx in indexes:
-            tree[idx] = colored_dot(color) if off else "\u001b[37m◍\033[32m"
+            tree[idx] = colored_dot(color) if off else "\u001b[37m●\033[32m"
 
         mutex.acquire()
         os.system("cls" if os.name == 'nt' else "clear")
@@ -60,23 +60,23 @@ def lights(color,indexes):
 for i,c in enumerate(tree):
     if c == "Y":
         yellow.append(i)
-        tree[i] = "◍"
+        tree[i] = "●"
     if c == "B":
         blue.append(i)
-        tree[i] = "◍"
+        tree[i] = "●"
     if c == "R":
         red.append(i)
-        tree[i] = "◍"
+        tree[i] = "●"
     if c == "W":
         bblue.append(i)
-        tree[i] = "◍"
+        tree[i] = "●"
 
 
 ty = threading.Thread(target=lights, args=("yellow",yellow))
 tb = threading.Thread(target=lights, args=("blue",blue))
 tr = threading.Thread(target=lights, args=("red",red))
 tw = threading.Thread(target=lights, args=("bblue",bblue))
-# print("".join(tree))
+
 
 for t in [ty,tb,tr,tw]:
     t.start()
